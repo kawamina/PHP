37a211760319d77e50304965c8a91780ad3c9560
Karen <42174311+kawamina@users.noreply.github.com>
2020-01-19 13:02:58 +0100

    Add files via upload

---
+++ site1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+
+      <?php
+        $nums = array(5,6,23,53,7,98);
+        for($i=0; $i<=count($nums); $i++){
+          echo "$nums[$i] <br>";
+        }
+      ?>
+  </body>
+</html>
